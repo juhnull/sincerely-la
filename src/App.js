9bd73e2c765fe467c79c79c20a4d278e0f94de31
juhnull <sc8bynut@gmail.com>
2022-02-20 00:19:43 -0800

    adding background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import hihi from './imgs/metroLA.png';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           hi hi hi
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div >
+      Hello World
+      </div>
+
     </div>
+
   );
 }
 
